cecf8492fb8fdecce6fe206b7ed39a8377bb88e2
Nick Perugini <nickperugini10@gmail.com>
2025-05-29 13:21:47 -0400

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,5 +1,17 @@
 resource "aws_lambda_function" "this" {
-  function_name = var.function_name
+  function_name = this_lambda
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip.output_path
+  vpc_config {
+    subnet_ids         = var.vpc_subnet_ids
+    security_group_ids = var.security_group_ids
+  }
+}
+
+resource "aws_lambda_function" "this_second" {
+  function_name = this_second_lambda
   role          = aws_iam_role.lambda_exec.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
